8a461b2be30b0ea614e387d3522848eb59aef9e5
magvin1 <magliovi@hotmail.it>
2025-11-26 12:39:32 +0100

    copia non funzionante di esercizio 1

---
+++ 03 - Linguaggio JS/esercitazioni/esercitazione-01.js
@@ -0,0 +1,34 @@
+//Esercizio 1
+let giorno=window.prompt("Inserisci il numero del giorno");//prompt restituisce una stringa
+let mese=window.prompt("Inserisci il numero del mese");//prompt restituisce una stringa
+let anno=window.prompt("Inserisci il numero dell'anno");//prompt restituisce una stringa
+/*
+//document.getElementById("es1-dataInserita").innerText= giorno + "/" + mese + "/" + anno;
+let dataFutura;
+
+if(anno<2025){
+    document.getElementById("es1-risultato").innerText="data è precedente all'attuale";
+}
+else if(anno>2025){
+    document.getElementById("es1-risultato").innerText="data successiva ad attuale";
+}
+else{ //anno==2025
+    if(mese<11){
+        document.getElementById("es1-risultato").innerText="data è precedente all'attuale";
+    }
+    else if(mese=12){
+        document.getElementById("es1-risultato").innerText="data successiva ad attuale";
+    }
+    else{ //mese==11
+        if(giorno<26){
+            document.getElementById("es1-risultato").innerText="data è precedente all'attuale";
+        }
+        else if(giorno>26){
+            document.getElementById("es1-risultato").innerText="data successiva ad attuale";
+        }
+        else{ //giorno==26
+            document.getElementById("es1-risultato").innerText="data corrisponde ad attuale";
+        }
+    }
+}
+*/
