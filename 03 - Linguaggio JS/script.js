eacc7cacde95ac8c787452aa7ad4f51f6b67466a
magvin1 <magliovi@hotmail.it>
2025-11-26 10:12:52 +0100

    Aggiunto a index.html di linguaggi JS il titolo h1 e modificato con commenti e nuovi metodi script.js

--- 03 - Linguaggio JS/script.js
+++ 03 - Linguaggio JS/script.js
@@ -1,2 +1,10 @@
+// commento singolo linea
+/* commento multilinea*/
+
 console.log("Ciao, mondo!");
 // Indica di logghare un messaggio nella console del browser Ciao Mondo
+window.alert("Benvenuto!");
+// Pop up di conferma di benvenuto all'utente
+console.log(window.confirm("Sei sicuro di voler continuare?"));
+// Pop up di conferma con due opzioni OK e Annulla
+// Aggiungegndo console.log prima di window.confirm possiamo vedere il valore booleano restituito dalla conferma
